7094700f9fbc82ad2142f11af693c1f5d0b85100
mateojeannesson <mateojeannesson@gmail.com>
2023-08-09 23:48:47 +0200

    tmp

--- lib/pdf_analyser.py
+++ lib/pdf_analyser.py
@@ -1,6 +1,7 @@
 import os
 
 from PyPDF2 import PdfReader
+import sys
 
 from lib.athlete_result import AthleteResult
 
@@ -8,8 +9,10 @@ from lib.athlete_result import AthleteResult
 class PdfAnalyser:
     def analayse_pdf(self, pdf_name):
         reader = PdfReader(f"data/{pdf_name}")
+        athlete_list = []
         for page in reader.pages:
-            self.analyse_page(page)
+            athlete_list += self.analyse_page(page)
+        return athlete_list
 
     def analyse_page(self, page):
         text = page.extract_text()
@@ -22,10 +25,16 @@ class PdfAnalyser:
             elif line.endswith("B:") and len(line.split(".")) == 8:
                 start_of_athlete_line.append(index - 1)
         start_of_athlete_line.append(len(text.split("\n")))
+        athlete_list = []
         for i in range(len(start_of_athlete_line) - 1):
-            athlete_line = "\n".join(text.split("\n")[start_of_athlete_line[i] : start_of_athlete_line[i + 1]])
+            athlete_line = "\n".join(
+                text.split("\n")[
+                    start_of_athlete_line[i] : start_of_athlete_line[i + 1]
+                ]
+            )
             athlete = AthleteResult(string=athlete_line, qualification=qualification)
             print(athlete)
+        return athlete_list
 
 
 if __name__ == "__main__":
